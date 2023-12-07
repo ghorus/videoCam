d581e8ba3fea542b4db4a2402c5473641f8d8ed9
ghorus <nguyen.victor4@gmail.com>
2023-12-07 00:32:38 -0800

    streamlit

--- app.py
+++ app.py
@@ -1,37 +1,10 @@
-# import eventlet
-# import eventlet.wsgi
-# eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
-import cv2
-from flask import Flask, render_template,Response
-
+from streamlit_webrtc import webrtc_streamer
+from flask import Flask, render_template
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+
+webrtc_streamer(key="key")
 
 @app.route("/")
 def home():
     return render_template('home.html')
-
-f = ""
-def generate_frames():
-    camera=cv2.VideoCapture(2, cv2.CAP_V4L2)
-    while True:
-        #read camera frame
-        success,frame=camera.read()
-        f = frame
-        if not success:
-            break
-        else:
-            ret,buffer=cv2.imencode('.jpeg',frame)
-            frame = buffer.tobytes()
-        yield(b'--frame\r\n' 
-              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-@app.route("/video")
-def video():
-    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
-if __name__ == '__main__':
-    app.run(debug=True)
-
-@app.route("/capture")
-def capture():
-    cv2.imwrite('webcam.jpeg',f)
