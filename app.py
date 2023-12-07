c98816c15a390c2d97f336ac2a80b2d467e038a0
ghorus <nguyen.victor4@gmail.com>
2023-12-07 00:46:12 -0800

    streamlit again

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import webrtc_streamer,RTCConfiguration
 from flask import Flask, render_template
+import av
+import cv2
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 
-webrtc_streamer(key="key")
+
+class VideoProcessor:
+    def recv(self,frame):
+        frm = frame.to_ndarray(format="bgr24")
+        return av.VideoFrame.from_ndarray(frm,format='bgr24')
+webrtc_streamer(key="key",video_processor_factory=VideoProcessor,rtc_configuration=RTCConfiguration(
+    {"iceServers":[{"urls":["stun:stun.l.google.com:19302"]}]}))
 
 @app.route("/")
 def home():
