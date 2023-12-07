a75281a54557c511aca265abad9ed007546c56a6
ghorus <nguyen.victor4@gmail.com>
2023-12-06 23:46:14 -0800

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+# import eventlet
+# import eventlet.wsgi
+# eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+import cv2
+from flask import Flask, render_template,Response
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+
+camera=cv2.VideoCapture(1)
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+f = ""
+def generate_frames():
+    while True:
+        #read camera frame
+        success,frame=camera.read()
+        f = frame
+        if not success:
+            break
+        else:
+            ret,buffer=cv2.imencode('.jpeg',frame)
+            frame = buffer.tobytes()
+        yield(b'--frame\r\n' 
+              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route("/video")
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+if __name__ == '__main__':
+    app.run(debug=True)
+
+@app.route("/capture")
+def capture():
+    cv2.imwrite('webcam.jpeg',f)
